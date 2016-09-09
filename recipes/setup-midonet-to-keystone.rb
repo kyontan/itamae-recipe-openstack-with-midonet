0b9c412163cf27b7d5bd1bd47a4ce412e34e4969
kyontan <kyontan@monora.me>
2016-09-09 11:04:47 +0900

    Fix setup-midonet-to-keystone.rb

--- recipes/setup-midonet-to-keystone.rb
+++ recipes/setup-midonet-to-keystone.rb
@@ -1,3 +1,5 @@
+env = "OS_TOKEN=#{node[:keystone_admin_token]} OS_URL=http://#{node[:controller_node_ip]}:35357/v3 OS_IDENTITY_API_VERSION=3"
+
 execute "Create the service entity for the midonet service" do
 	command "#{env} openstack service create --name midonet --description \"MidoNet API Service\" midonet"
 	not_if "#{env} openstack service list | grep midonet"
